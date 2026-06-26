aee34ccb02a85f4dc2dbd62b669f796f92ecc7db
Dinh Nguyen <32100818+dinhtnguyenn@users.noreply.github.com>
2026-06-26 07:29:52 +0700

    up

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'unifolio-images-7143dba';
+const CACHE_NAME = 'unifolio-images-d03e39a';
 const SAME_ORIGIN_PREFIXES = ['/assets/', '/images/'];
 const EXTERNAL_IMAGE_HOSTS = new Set(['i.ytimg.com', 'img.youtube.com']);
 
