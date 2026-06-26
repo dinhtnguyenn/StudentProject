dce7996759e28e8236d003c7b8d0eae30969063b
Dinh Nguyen <32100818+dinhtnguyenn@users.noreply.github.com>
2026-06-26 10:13:27 +0700

    up

--- fix_quotes.py
+++
@@ -1,8 +0,0 @@
-with open('/Users/dinhnguyen/Desktop/StudentProject/src/components/AdminForm.tsx', 'r') as f:
-    content = f.read()
-
-content = content.replace(r"\'grab\'", "'grab'")
-content = content.replace(r"\'not-allowed\'", "'not-allowed'")
-
-with open('/Users/dinhnguyen/Desktop/StudentProject/src/components/AdminForm.tsx', 'w') as f:
-    f.write(content)
